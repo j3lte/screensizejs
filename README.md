32cbcf0b9451ea71f36f39ad7abc13222bae3fdd
j3lte <jwlagendijk@gmail.com>
2013-09-24 15:56:06 +0200

    IE fixes

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ screensizejs
 ============
 
 Just a small script you can insert to view your screensize in EM
+
+## Getting Started
+
+Insert the script in the page
+
+```
+  <script type="text/javascript" src="./screensize.js"></script>
+```
+
+## Usage
+
+Make sure you reset the font-size on the body tag
+
+```
+  <style type="text/css">
+    body {
+      font-size:16px;
+    }
+  </style>
+```
--- screensize.js
+++ screensize.js
@@ -10,9 +10,9 @@
     function init() {
         addEl();
         handleResize();
-        window.addEventListener("resize", function () {
-                 if (timeOut !== null)
-                        clearTimeout(timeOut);
+        bindEvent(window, "resize", function () {
+                if (timeOut !== null)
+                    clearTimeout(timeOut);
                 timeOut = setTimeout(handleResize, 250);
         });
     }
@@ -24,10 +24,10 @@
     function addEl () {
         var _body = document.getElementsByTagName('body') [0],
                 el = document.createElement("div");
-        fontSize = parseInt(getComputedStyle(document.getElementsByTagName('body')[0],null).fontSize || getComputedStyle(document.body,0).fontSize,10);
+        fontSize = parseInt(window.getComputedStyle(document.getElementsByTagName('body')[0], null).getPropertyValue("font-size") || window.getComputedStyle(document.body, null).getPropertyValue("font-size"), 10);
 
         el.id = 'screensize';
-        el.setAttribute("style","background: rgba(0, 0, 0, 0.2); color: #000; font-weight: 700; padding: 2px; position: fixed; right: 5px; top: 5px; z-index: 1000;");
+        el.setAttribute("style", "background: rgba(0, 0, 0, 0.2); color: #000; font-weight: 700; padding: 2px; position: fixed; right: 5px; top: 5px; z-index: 1000;");
 
         _body.appendChild(el);
     }
@@ -37,9 +37,41 @@
      * @return {void}
      */
     function handleResize() {
-      document.getElementById("screensize").innerHTML = "Fontsize: " + fontSize + "px, <br /> Screensize: " + parseInt(window.innerWidth / fontSize, 10) + " x " + parseInt(window.innerHeight / fontSize, 10) + " em";
+        var innerWidth = window.innerWidth || document.documentElement.clientWidth
+            innerHeight = window.innerWidth || document.documentElement.clientHeight;
+        document.getElementById("screensize").innerHTML = "Fontsize: " + fontSize + "px, <br /> Screensize: " + parseInt(innerWidth / fontSize, 10) + " x " + parseInt(innerHeight / fontSize, 10) + " em";
     }
 
-    window.addEventListener("load", init);
+    bindEvent(window, "load", init);
 
 })();
+
+/**
+ * IE Compatibility fix bindEvent
+ */
+function bindEvent(el, eventName, eventHandler) {
+    if (el.addEventListener){
+        el.addEventListener(eventName, eventHandler, false); 
+    } else if (el.attachEvent){
+        el.attachEvent('on'+eventName, eventHandler);
+    }
+}
+/**
+ * IE Compatibility fix getComputedStyle
+ */
+if (!window.getComputedStyle) {
+    window.getComputedStyle = function(el, pseudo) {
+        this.el = el;
+        this.getPropertyValue = function(prop) {
+            var re = /(\-([a-z]){1})/g;
+            if (prop == 'float') prop = 'styleFloat';
+            if (re.test(prop)) {
+                prop = prop.replace(re, function () {
+                    return arguments[2].toUpperCase();
+                });
+            }
+            return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+        }
+        return this;
+    }
+}
