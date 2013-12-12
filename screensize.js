4b275c70183e78b2cb2d68ae01d173f0a291bf83
j3lte <jwlagendijk@gmail.com>
2013-12-12 16:19:46 +0100

    JSLint + JSHint compatible, plus minified

--- screensize.js
+++ screensize.js
@@ -1,35 +1,65 @@
-(function() {
-    
+/*jslint browser: true, unparam: true */
+
+/**
+ * IE Compatibility fix bindEvent
+ */
+function bindEvent(el, eventName, eventHandler) {
+    'use strict';
+    if (el.addEventListener) {
+        el.addEventListener(eventName, eventHandler, false);
+    } else if (el.attachEvent) {
+        el.attachEvent('on' + eventName, eventHandler);
+    }
+}
+
+/**
+ * IE Compatibility fix getComputedStyle
+ */
+if (!window.getComputedStyle) {
+    window.getComputedStyle = function (el, pseudo) {
+        'use strict';
+        this.el = el;
+        this.getPropertyValue = function (prop) {
+            var re = /(\-([a-z]){1})/g;
+            if (prop === 'float') {
+                prop = 'styleFloat';
+            }
+            if (re.test(prop)) {
+                prop = prop.replace(re, function (a, b, c) {
+                    return c.toUpperCase();
+                });
+            }
+            if (el.currentStyle[prop]) {
+                return el.currentStyle[prop];
+            }
+            return null;
+        };
+        return this;
+    };
+}
+
+/**
+ * Self executing
+ */
+
+(function () {
+    'use strict';
     var fontSize,
         timeOut = null;
 
     /**
-     * Start the code and add an event listener to resize. Only fire handleResize 250ms after stop resizing
-     * @return {void}
-     */
-    function init() {
-        addEl();
-        handleResize();
-        bindEvent(window, "resize", function () {
-                if (timeOut !== null)
-                    clearTimeout(timeOut);
-                timeOut = setTimeout(handleResize, 250);
-        });
-    }
-    
-    /**
      * Add the screensize element to body and calculates font-size
      * @return {void}
      */
-    function addEl () {
-        var _body = document.getElementsByTagName('body') [0],
-                el = document.createElement("div");
+    function addEl() {
+        var body = document.getElementsByTagName('body')[0],
+            el = document.createElement("div");
         fontSize = parseInt(window.getComputedStyle(document.getElementsByTagName('body')[0], null).getPropertyValue("font-size") || window.getComputedStyle(document.body, null).getPropertyValue("font-size"), 10);
 
         el.id = 'screensize';
         el.setAttribute("style", "background: rgba(0, 0, 0, 0.2); color: #000; font-weight: 700; padding: 2px; position: fixed; right: 5px; top: 5px; z-index: 1000;");
 
-        _body.appendChild(el);
+        body.appendChild(el);
     }
 
     /**
@@ -42,36 +72,22 @@
         document.getElementById("screensize").innerHTML = "Fontsize: " + fontSize + "px, <br /> Screensize: " + parseInt(innerWidth / fontSize, 10) + " x " + parseInt(innerHeight / fontSize, 10) + " em";
     }
 
+
+    /**
+     * Start the code and add an event listener to resize. Only fire handleResize 250ms after stop resizing
+     * @return {void}
+     */
+    function init() {
+        addEl();
+        handleResize();
+        bindEvent(window, "resize", function () {
+            if (timeOut !== null) {
+                clearTimeout(timeOut);
+            }
+            timeOut = setTimeout(handleResize, 250);
+        });
+    }
+
     bindEvent(window, "load", init);
 
-})();
-
-/**
- * IE Compatibility fix bindEvent
- */
-function bindEvent(el, eventName, eventHandler) {
-    if (el.addEventListener){
-        el.addEventListener(eventName, eventHandler, false); 
-    } else if (el.attachEvent){
-        el.attachEvent('on'+eventName, eventHandler);
-    }
-}
-/**
- * IE Compatibility fix getComputedStyle
- */
-if (!window.getComputedStyle) {
-    window.getComputedStyle = function(el, pseudo) {
-        this.el = el;
-        this.getPropertyValue = function(prop) {
-            var re = /(\-([a-z]){1})/g;
-            if (prop == 'float') prop = 'styleFloat';
-            if (re.test(prop)) {
-                prop = prop.replace(re, function () {
-                    return arguments[2].toUpperCase();
-                });
-            }
-            return el.currentStyle[prop] ? el.currentStyle[prop] : null;
-        }
-        return this;
-    }
-}
+}());
